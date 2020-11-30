1b860fe70040cbfd6adaa66b88fedf2ed084ceaf
irtep <rasanen.petri@gmail.com>
2020-11-30 16:53:22 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          HordesHelp 2
       </header>
+      <div id = "controlButtons">{/*up*/}
+      </div>
+      <div id = "feedBack">{/*up*/}
+      </div>
+      <div id = "setups">{/*up left*/}
+      </div>
+      <div id = "selectedArmy">{/*left side*/}
+      </div>
+      <div id = "unitsAvailable">{/*right side*/}
+      </div>
+
     </div>
   );
 }
