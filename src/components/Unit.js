443ce40b3ceb8d1213f251386197cfc013bd1963
irtep <rasanen.petri@gmail.com>
2020-12-04 21:14:14 +0200

    updt

--- src/components/Unit.js
+++ src/components/Unit.js
@@ -2,22 +2,32 @@ import React, { Component } from "react";
 import '../App.css';
 
 class Unit extends Component {
+  constructor() {
+    super();
+    this.clicked = this.clicked.bind(this);
+  }
+  componentDidMount() {
+    //console.log('unit mounted ', this.props);
+  }
+  clicked(e) {
+    console.log('clicked: ', e.target.id);
+  }
   render() {
-    let showThis = '';
-    if (this.props.showing !== '') {
-      showThis = '';
-      this.props.cards.forEach((item, i) => {
-        item.factions.forEach((it, ii) => {
-          if (this.props.showing === it) {
-            console.log('found: ', this.props.showing, item.name);
-            showThis = item.name;
-          }
-        });
-      });
-    }
+    let cardToShow = '';
+    let cardsId = '';
+    // check if this is card for this faction
+    this.props.title.factions.forEach((item, i) => {
+      if (item === this.props.factionShowing) {
+        cardToShow = `${this.props.title.name} (${this.props.title.type})
+        point cost: ${this.props.title.pointCost}`;
+        cardsId = this.props.title.name;
+      }
+    });
     return(
-      <div>
-       {showThis}
+      <div className = "cards">
+        <div id = {cardsId} onClick = {this.clicked}>
+          {cardToShow}
+        </div>
       </div>
     );
   }
